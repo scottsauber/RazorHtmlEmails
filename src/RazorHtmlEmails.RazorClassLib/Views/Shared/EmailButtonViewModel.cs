e56dff2313ba67f30d8df75e5c0ddd9446cd4de0
Scott Sauber <stsauber@gmail.com>
2018-07-05 21:13:29 -0500

    Add Email Button

---
+++ src/RazorHtmlEmails.RazorClassLib/Views/Shared/EmailButtonViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace RazorHtmlEmails.RazorClassLib.Views.Shared
+{
+    public class EmailButtonViewModel
+    {
+        public EmailButtonViewModel(string text, string url)
+        {
+            Text = text;
+            Url = url;
+        }
+
+        public string Text { get; set; }
+        public string Url { get; set; }
+    }
+}
