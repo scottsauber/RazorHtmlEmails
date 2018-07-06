c9febaf74fe8811c7625107ad9f73e4e71603c3b
Scott Sauber <stsauber@gmail.com>
2018-07-05 22:17:32 -0500

    Add Confirm Account Email

---
+++ src/RazorHtmlEmails.RazorClassLib/Views/Emails/ConfirmAccount/ConfirmAccountEmailViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace RazorHtmlEmails.RazorClassLib.Views.Emails.ConfirmAccount
+{
+    public class ConfirmAccountEmailViewModel
+    {
+        public ConfirmAccountEmailViewModel(string confirmEmailUrl)
+        {
+            ConfirmEmailUrl = confirmEmailUrl;
+        }
+
+        public string ConfirmEmailUrl { get; set; }
+    }
+}
