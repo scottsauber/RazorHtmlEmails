ab0d09017c60fb6e5636e1db1c779dc567f10714
Scott Sauber <stsauber@gmail.com>
2021-12-31 19:52:22 -0600

    update to use records

--- src/RazorHtmlEmails.RazorClassLib/Views/Emails/ConfirmAccount/ConfirmAccountEmailViewModel.cs
+++ src/RazorHtmlEmails.RazorClassLib/Views/Emails/ConfirmAccount/ConfirmAccountEmailViewModel.cs
@@ -1,11 +1,3 @@
 ﻿namespace RazorHtmlEmails.RazorClassLib.Views.Emails.ConfirmAccount;
 
-public class ConfirmAccountEmailViewModel
-{
-    public ConfirmAccountEmailViewModel(string confirmEmailUrl)
-    {
-        ConfirmEmailUrl = confirmEmailUrl;
-    }
-
-    public string ConfirmEmailUrl { get; set; }
-}
+public record ConfirmAccountEmailViewModel(string ConfirmEmailUrl);
--- src/RazorHtmlEmails.RazorClassLib/Views/Shared/EmailButtonViewModel.cs
+++ src/RazorHtmlEmails.RazorClassLib/Views/Shared/EmailButtonViewModel.cs
@@ -1,13 +1,3 @@
 ﻿namespace RazorHtmlEmails.RazorClassLib.Views.Shared;
 
-public class EmailButtonViewModel
-{
-    public EmailButtonViewModel(string text, string url)
-    {
-        Text = text;
-        Url = url;
-    }
-
-    public string Text { get; set; }
-    public string Url { get; set; }
-}
+public record EmailButtonViewModel(string Text, string Url);
